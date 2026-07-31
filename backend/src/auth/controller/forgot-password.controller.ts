23243d7045a6031e9cb4ea4fe036223546beb00a
Pedro Augusto <perobr2018@gmail.com>
2026-07-31 15:26:35 -0300

    fix - Correcting the receive of email and passage of fields for structuring of values referred code otp

--- backend/src/auth/controller/forgot-password.controller.ts
+++ backend/src/auth/controller/forgot-password.controller.ts
@@ -4,22 +4,23 @@ import { forgotPasswordSchema } from "../schema/forgot-password.schema";
 import { otpSchema } from "../schema/otp.schema";
 import { forgotPasswordService } from "../service/forgot-password.service";
 import NodemailerHelper from 'nodemailer-otp';
-import bcrypt from "bcrypt-node";
+import bcrypt from "bcrypt";
 
 export const forgotPasswordController: RequestHandler = async (req, res, next) => {
   try{
       const data = forgotPasswordSchema.parse(req.body);
-      const result = forgotPasswordService(data.email);
+      const result = await forgotPasswordService(data.email);
 
       if(!result) return new AppError("email not exists", 404);
 
       const helper = new NodemailerHelper("exemple@gmail.com", "passemailexample");
       const otp = helper.generateOtp(6);  
-      
+      const hashOtp = await bcrypt.hash(otp, 10);
+
       const object_otp = otpSchema.parse({
-         otp: bcrypt.hashSync(otp),
-         time_expire: 300000,
-         moment_generate: Date.now()
+         code_otp: hashOtp,
+         moment_generate: new Date().toISOString(),
+         moment_expire: 300000
       });
    
       //just while email for otp don't been configured!
@@ -30,6 +31,6 @@ export const forgotPasswordController: RequestHandler = async (req, res, next) =
       });
   }
   catch(error){
-      Hext(error);
+      return console.log(error);
   }
 }
